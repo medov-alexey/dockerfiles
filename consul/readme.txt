cb8c08306462a149bb26082977ac6467385837cb
Alexey Medov <amedov@gitlab.int>
2022-03-17 23:29:50 +0300

    new fix

--- consul/readme.txt
+++ consul/readme.txt
@@ -1,1 +1,19 @@
-Bash script for run simple instance Consul by HashiCorp in Docker container
+# Bash script for run simple single instance Consul by HashiCorp in Docker container.
+
+# This is not a Consul Cluster. Only one node.
+
+#-----------------------------------------------------------------------------------
+
+# For run Consul use this command:
+
+./run.sh
+
+# or this command
+
+$(pwd)/run.sh
+
+#-----------------------------------------------------------------------------------
+
+# For delete Consul container use this command:
+
+docker rm -f consul
--- consul/run.sh
+++ consul/run.sh
@@ -12,12 +12,16 @@ TOKEN=725e5217-9412-5da0-b5d5-f60afc02cac8
 #
 # And then execute ./run.sh
 #
+#---------------------------------------------------
+version=1.11
+#---------------------------------------------------
 
-echo '{"datacenter":"dc1","primary_datacenter":"dc1","server":true,"bootstrap_expect":1,"data_dir":"/consul/data","log_level":"INFO","node_name":"consul","client_addr":"0.0.0.0","ui_config":{"enabled":true},"telemetry":{"disable_compat_1.9":true},"acl":{"enabled":true,"default_policy":"deny","enable_token_persistence":true,"tokens":{"initial_management":"$TOKEN","agent":"$TOKEN"},"down_policy":"extend-cache"}}' | sed 's/$TOKEN/'$TOKEN'/g'> /tmp/consul-config.json
 
-docker run -d --name consul -p 80:8500 -e CONSUL_LOCAL_CONFIG=$(cat /tmp/consul-config.json) -e CONSUL_HTTP_ADDR=127.0.0.1:8500 -e CONSUL_HTTP_TOKEN=$TOKEN consul:1.11 > /dev/null 2>&1
+echo '{"datacenter":"dc1","primary_datacenter":"dc1","server":true,"data_dir":"/consul/data","log_level":"INFO","node_name":"consul","client_addr":"0.0.0.0","ui_config":{"enabled":true},"telemetry":{"disable_compat_1.9":true},"acl":{"enabled":true,"default_policy":"deny","enable_token_persistence":true,"tokens":{"initial_management":"$TOKEN","agent":"$TOKEN"},"down_policy":"extend-cache"}}' | sed 's/$TOKEN/'$TOKEN'/g'> /tmp/consul-config.json
 
-sleep 3
+docker run -d --name consul -p 80:8500 -e CONSUL_LOCAL_CONFIG=$(cat /tmp/consul-config.json) -e CONSUL_HTTP_ADDR=127.0.0.1:8500 -e CONSUL_HTTP_TOKEN=$TOKEN consul:$version agent -server -bootstrap-expect=1 > /dev/null 2>&1
+
+sleep 10
 
 echo
 
@@ -27,6 +31,6 @@ echo
 
 docker ps -a --filter "status=exited" | grep consul > /dev/null 2>&1 && echo && echo "Container with Consul was deleted, because he is not started" && docker rm -f consul > /dev/null 2>&1 && echo
 
-docker ps | grep consul > /dev/null 2>&1 && echo && echo "Go to link --> http://localhost and use this token --> $TOKEN <-- for authorization" && echo && echo "Succesfully =)" && echo && echo
+docker ps | grep consul > /dev/null 2>&1 && echo && echo "Succesfully =)" && echo && echo "Go to link --> http://localhost and use this token --> $TOKEN <-- for authorization" && echo && echo && sleep 5 && docker exec -it consul consul members && echo && docker exec -it consul consul operator raft list-peers
 
 rm -rf /tmp/consul-config.json
